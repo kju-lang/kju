5fdf42dcd31396e5050e951fc1dadb5e26fd14fb
Bartlomiej Puget <larhard@gmail.com>
2019-03-05 11:05:58 +0100

    Set LICENSE

--- src/KJU.Core/HelloWorld.cs
+++ src/KJU.Core/HelloWorld.cs
@@ -1,5 +1,7 @@
 ﻿// <copyright file="HelloWorld.cs" company="KJU Supreme Language Development Team">
 // Copyright (c) KJU Supreme Language Development Team. All rights reserved.
+// Licensed under the BSD License 2.0. See LICENSE file in the project root for
+// full license information.
 // </copyright>
 
 namespace KJU.Core
