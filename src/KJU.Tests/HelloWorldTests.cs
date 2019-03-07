d31aacc2a5ce24653e28ade934b9c939c0e5ac27
Rafał Burczyński <merfik18@gmail.com>
2019-03-07 21:39:59 +0000

    Add Moq to project dependencies
    Also introduce simple unit test to ensure that package is installed
    properly; longish diff is a result of CRLF->LF line ends conversion.

--- src/KJU.Tests/HelloWorldTests.cs
+++ src/KJU.Tests/HelloWorldTests.cs
@@ -1,7 +1,9 @@
 namespace KJU.Tests
 {
+    using System;
     using KJU.Core;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
 
     /// <summary>
     /// Example tests class.
@@ -17,6 +19,14 @@ namespace KJU.Tests
         {
             KJU.Core.HelloWorld helloWorld = new KJU.Core.HelloWorld();
             Assert.AreEqual("Hello World!", helloWorld.Hello());
+        }
+
+        [TestMethod]
+        public void TestHelloWorldMock()
+        {
+            Mock<Func<int, int>> mock = new Mock<Func<int, int>>();
+            mock.Setup(m => m(0)).Returns(1);
+            Assert.AreEqual(1, mock.Object(0));
         }
     }
 }
