748c440ad12c91fc6a64d6bb9c44e0871169c398
Jan Derbisz <jan.derbisz@gmail.com>
2019-03-15 11:52:55 +0000

    Implement parsing

--- src/KJU.Core/Parser/Parser.cs
+++ src/KJU.Core/Parser/Parser.cs
@@ -6,16 +6,119 @@ namespace KJU.Core.Parser
     using KJU.Core.Input;
     using KJU.Core.Lexer;
     using KJU.Core.Regex;
+    using KJU.Core.Util;
 
     public class Parser<TLabel>
     {
-        public Parser(CompiledGrammar<TLabel> grammar, IReadOnlyDictionary<Tuple<DfaAndState<TLabel>, TLabel>, ParseAction<TLabel>> table)
+        private CompiledGrammar<TLabel> grammar;
+        private IReadOnlyDictionary<Tuple<IState, IDfa<Optional<Rule<TLabel>>, TLabel>, TLabel>, ParseAction<TLabel>> table;
+
+        public Parser(CompiledGrammar<TLabel> grammar, IReadOnlyDictionary<Tuple<IState, IDfa<Optional<Rule<TLabel>>, TLabel>, TLabel>, ParseAction<TLabel>> table)
         {
+            this.grammar = grammar;
+            this.table = table;
         }
 
-        public ParseTree<TLabel> Parse(IEnumerable<TLabel> tokens)
+        public ParseTree<TLabel> Parse(IEnumerable<Token<TLabel>> tokens)
         {
-            throw new NotImplementedException();
+            TLabel start = this.grammar.StartSymbol;
+            IReadOnlyDictionary<TLabel, IDfa<Optional<Rule<TLabel>>, TLabel>> rules = this.grammar.Rules;
+            Stack<IState> statesStack = new Stack<IState>();
+            Stack<IDfa<Optional<Rule<TLabel>>, TLabel>> dfaStack = new Stack<IDfa<Optional<Rule<TLabel>>, TLabel>>();
+            Stack<List<ParseTree<TLabel>>> nodesStack = new Stack<List<ParseTree<TLabel>>>();
+
+            statesStack.Push(rules[start].StartingState());
+            dfaStack.Push(rules[start]);
+            nodesStack.Push(new List<ParseTree<TLabel>>());
+
+            ParseTree<TLabel> root = null;
+
+            IEnumerator<Token<TLabel>> enumerator = tokens.GetEnumerator();
+            enumerator.MoveNext();
+            while (statesStack.Count != 0)
+            {
+                Token<TLabel> token = enumerator.Current;
+                IState state = statesStack.Peek();
+                IDfa<Optional<Rule<TLabel>>, TLabel> dfa = dfaStack.Peek();
+                ParseAction<TLabel> action = this.table[new Tuple<IState, IDfa<Optional<Rule<TLabel>>, TLabel>, TLabel>(state, dfa, token.Category)];
+                if (action.Kind == ParseAction<TLabel>.ActionKind.Shift)
+                {
+                    nodesStack.Peek().Add(token);
+                    statesStack.Pop();
+                    statesStack.Push(dfa.Transitions(state)[token.Category]);
+                    if (!enumerator.MoveNext())
+                    {
+                        while (true)
+                        {
+                            state = statesStack.Peek();
+                            dfa = dfaStack.Peek();
+
+                            if (dfa.Label(state).IsNone())
+                            {
+                                throw new Exception("Invalid reduce action");
+                            }
+
+                            Brunch<TLabel> brunch = new Brunch<TLabel>();
+                            brunch.Rule = dfa.Label(state).Get();
+                            brunch.Children = nodesStack.Peek();
+                            brunch.Category = brunch.Rule.Lhs;
+                            Range range = new Range();
+                            range.Begin = nodesStack.Peek()[0].InputRange.Begin;
+                            range.End = nodesStack.Peek()[nodesStack.Peek().Count - 1].InputRange.End;
+                            brunch.InputRange = range;
+
+                            statesStack.Pop();
+                            dfaStack.Pop();
+                            nodesStack.Pop();
+
+                            root = brunch;
+
+                            if (nodesStack.Count == 0)
+                            {
+                                return root;
+                            }
+                        }
+                    }
+                }
+                else if (action.Kind == ParseAction<TLabel>.ActionKind.Reduce)
+                {
+                    if (dfa.Label(state).IsNone())
+                    {
+                        throw new Exception("Invalid reduce action");
+                    }
+
+                    Brunch<TLabel> brunch = new Brunch<TLabel>();
+                    brunch.Rule = dfa.Label(state).Get();
+                    brunch.Children = nodesStack.Peek();
+                    brunch.Category = brunch.Rule.Lhs;
+                    Range range = new Range();
+                    range.Begin = nodesStack.Peek()[0].InputRange.Begin;
+                    range.End = nodesStack.Peek()[nodesStack.Peek().Count - 1].InputRange.End;
+                    brunch.InputRange = range;
+
+                    statesStack.Pop();
+                    dfaStack.Pop();
+                    nodesStack.Pop();
+
+                    root = brunch;
+
+                    if (nodesStack.Count != 0)
+                    {
+                        nodesStack.Peek().Add(root);
+                    }
+                }
+                else if (action.Kind == ParseAction<TLabel>.ActionKind.Call)
+                {
+                    TLabel symbol = action.Label;
+                    statesStack.Pop();
+                    statesStack.Push(dfa.Transitions(state)[symbol]);
+                    dfaStack.Push(rules[symbol]);
+                    statesStack.Push(rules[symbol].StartingState());
+                    nodesStack.Push(new List<ParseTree<TLabel>>());
+                }
+            }
+
+            throw new Exception("Parsing finished before reading all tokens");
         }
     }
 }
---
+++ src/KJU.Tests/Parser/ParseTests.cs
@@ -0,0 +1,107 @@
+﻿namespace KJU.Tests.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using KJU.Core;
+    using KJU.Core.Automata;
+    using KJU.Core.Automata.NfaToDfa;
+    using KJU.Core.Lexer;
+    using KJU.Core.Parser;
+    using KJU.Core.Util;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class ParseTests
+    {
+        private enum Label
+        {
+            A, B
+        }
+
+        private class IntState : IState
+        {
+            private readonly int i;
+
+            public IntState(int i)
+            {
+                this.i = i;
+            }
+
+            public bool Equals(IState other)
+            {
+                return this.Equals(other as object);
+            }
+
+            public override bool Equals(object other)
+            {
+                if (!(other is IntState))
+                {
+                    return false;
+                }
+
+                return this.i == (other as IntState).i;
+            }
+
+            public override int GetHashCode()
+            {
+                return this.i;
+            }
+        }
+
+        private class IntDfa : IDfa<Optional<Rule<Label>>, Label>
+        {
+            public bool IsStable(IState state)
+            {
+                return false;
+            }
+
+            public Optional<Rule<Label>> Label(IState state)
+            {
+                return Optional<Rule<Label>>.None();
+            }
+
+            public IState StartingState()
+            {
+                IntState acc = new IntState(0);
+                return acc;
+            }
+
+            public IReadOnlyDictionary<Label, IState> Transitions(IState state)
+            {
+                Dictionary<Label, IState> edges = new Dictionary<Label, IState>();
+                Label l = ParseTests.Label.A;
+                edges.Add(l, new IntState(0));
+                l = ParseTests.Label.B;
+                edges.Add(l, new IntState(0));
+                return edges;
+            }
+        }
+
+        [TestMethod]
+#pragma warning disable SA1201 // Elements must appear in the correct order
+        public void ParseTest()
+#pragma warning restore SA1201 // Elements must appear in the correct order
+        {
+            var rules = new Dictionary<Label, IDfa<Optional<Rule<Label>>, Label>>();
+            IntDfa dfa = new IntDfa();
+            rules.Add(Label.A, dfa);
+            var grammar = new CompiledGrammar<Label>() { Rules = rules, StartSymbol = Label.A };
+            var parseTable = new Dictionary<Tuple<IState, IDfa<Optional<Rule<Label>>, Label>, Label>, ParseAction<Label>>();
+            parseTable.Add(new Tuple<IState, IDfa<Optional<Rule<Label>>, Label>, Label>(new IntState(0), dfa, Label.B), new ParseAction<Label>() { Kind = ParseAction<Label>.ActionKind.Shift });
+            Parser<Label> parser = new Parser<Label>(grammar, parseTable);
+
+            List<Token<Label>> tokens = new List<Token<Label>>();
+            tokens.Add(new Token<Label>() { Category = Label.B });
+            try
+            {
+                var root = parser.Parse(tokens);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Invalid reduce action");
+            }
+        }
+    }
+}
