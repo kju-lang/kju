f7f32eb57606f80b91bdcf705a21e83674389a89
Michał Zieliński <michal@zielinscy.org.pl>
2019-03-24 17:26:51 +0100

    use AST creator, name and type resolvers in Compiler

--- src/KJU.Application/Program.cs
+++ src/KJU.Application/Program.cs
@@ -3,6 +3,8 @@
     using System;
     using System.IO;
     using KJU.Core;
+    using KJU.Core.AST;
+    using KJU.Core.Compiler;
     using KJU.Core.Diagnostics;
     using KJU.Core.Input;
     using KJU.Core.Parser;
@@ -17,18 +19,16 @@
 
                 try
                 {
-                    string data = File.ReadAllText(filename);
-                    Console.WriteLine($"compiling {data}...");
-                    var tree = KjuParserFactory.Instance.Parse(data, diag);
-                    Console.WriteLine($"tree: {tree}");
+                    new Compiler().Run(filename, diag);
                 }
                 catch (Exception ex) when (
                        ex is ParseException
                     || ex is FormatException
                     || ex is PreprocessorException)
                 {
-                    diag.Report();
                 }
+
+                diag.Report();
             }
         }
     }
--- src/KJU.Core/Compiler/Compiler.cs
+++ src/KJU.Core/Compiler/Compiler.cs
@@ -1,17 +1,44 @@
 namespace KJU.Core.Compiler
 {
+    using System;
+    using System.IO;
+    using KJU.Core;
+    using KJU.Core.AST;
     using KJU.Core.Diagnostics;
+    using KJU.Core.Input;
+    using KJU.Core.Lexer;
+    using KJU.Core.Parser;
 
     public class Compiler : ICompiler
     {
-        /*Compiler(Lexer, Parser, ect...)
-        {
+        private INameResolver nameResolver;
+        private ITypeChecker typeChecker;
+        private IParseTreeToAstConverter<KjuAlphabet> parseTreeToAstConverter;
+        private Parser<KjuAlphabet> parser;
 
-        }*/
-
-        public void Run(string path, IDiagnostics diagnostics)
+        public Compiler(Parser<KjuAlphabet> parser, INameResolver nameResolver, ITypeChecker typeChecker, IParseTreeToAstConverter<KjuAlphabet> parseTreeToAstConverter)
         {
-            throw new System.NotImplementedException();
+            this.parser = parser;
+            this.nameResolver = nameResolver;
+            this.typeChecker = typeChecker;
+            this.parseTreeToAstConverter = parseTreeToAstConverter;
+        }
+
+        public Compiler()
+            : this(KjuParserFactory.Instance, new NameResolver(), new TypeChecker(), new KjuParseTreeToAstConverter())
+        {
+        }
+
+        public void Run(string path, IDiagnostics diag)
+        {
+            string data = File.ReadAllText(path);
+            Console.WriteLine($"compiling {data}...");
+            var tree = this.parser.Parse(data, diag);
+            Console.WriteLine($"tree: {tree}");
+            var ast = this.parseTreeToAstConverter.GenerateAst(tree, diag);
+            Console.WriteLine($"ast: {ast}");
+            this.nameResolver.LinkNames(ast, diag);
+            this.typeChecker.LinkTypes(ast, diag);
         }
     }
 }
