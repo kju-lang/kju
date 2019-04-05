519d2a406119976bc0d4d12046b8f307e9faaaaa
Jan Derbisz <jan.derbisz@gmail.com>
2019-04-05 18:27:27 +0000

    Implement Call Graph

--- src/KJU.Core/AST/CallGraphGenerator.cs
+++ src/KJU.Core/AST/CallGraphGenerator.cs
@@ -5,9 +5,42 @@ namespace KJU.Core.AST
 
     public class CallGraphGenerator
     {
+        private Dictionary<FunctionDeclaration, IReadOnlyCollection<FunctionDeclaration>> callGraph;
+
         public IReadOnlyDictionary<FunctionDeclaration, IReadOnlyCollection<FunctionDeclaration>> BuildCallGraph(Node root)
         {
-            throw new NotImplementedException();
+            this.callGraph = new Dictionary<FunctionDeclaration, IReadOnlyCollection<FunctionDeclaration>>();
+            this.ProcessNode(root);
+            return this.callGraph;
+        }
+
+        private IReadOnlyCollection<FunctionDeclaration> ProcessNode(Node node)
+        {
+            HashSet<FunctionDeclaration> funs = new HashSet<FunctionDeclaration>();
+
+            switch (node)
+            {
+                case FunctionDeclaration fun:
+                    this.callGraph.Add(fun, this.ProcessNode(fun.Body));
+                    break;
+                case FunctionCall call:
+                    foreach (var child in call.Children())
+                    {
+                        funs.UnionWith(this.ProcessNode(child));
+                    }
+
+                    funs.Add(call.Declaration);
+                    break;
+                default:
+                    foreach (var child in node.Children())
+                    {
+                        funs.UnionWith(this.ProcessNode(child));
+                    }
+
+                    break;
+            }
+
+            return funs;
         }
     }
 }
---
+++ src/KJU.Tests/AST/CallGraphTests.cs
@@ -0,0 +1,128 @@
+﻿namespace KJU.Tests.AST
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using KJU.Core.AST;
+    using KJU.Core.AST.BuiltinTypes;
+    using KJU.Core.Diagnostics;
+    using KJU.Tests.Util;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CallGraphTests
+    {
+        /*
+         * fun a()
+         * {
+         *   b();
+         * }
+         * fun b()
+         * {
+         *   b();
+         * }
+         * fun c()
+         * {
+         *   b();
+         * }
+         */
+        [TestMethod]
+        public void TestCallGraph()
+        {
+            var names = new List<string> { "a", "b", "c" };
+            var functions = new List<FunctionDeclaration>();
+            var calls = new List<FunctionCall>();
+            foreach (var id in names)
+            {
+                var functionCall = new FunctionCall("b", new List<Expression>());
+                calls.Add(functionCall);
+                var body = new InstructionBlock(new List<Expression> { functionCall });
+                var fun = new FunctionDeclaration(
+                    id,
+                    UnitType.Instance,
+                    new List<VariableDeclaration>(),
+                    body);
+                functions.Add(fun);
+            }
+
+            foreach (var call in calls)
+            {
+                call.Declaration = functions[1];
+            }
+
+            var root = new Program(functions);
+
+            CallGraphGenerator gen = new CallGraphGenerator();
+            var dict = gen.BuildCallGraph(root);
+
+            Assert.AreEqual(1, dict[functions[0]].Count);
+            Assert.AreEqual(1, dict[functions[1]].Count);
+            Assert.AreEqual(1, dict[functions[2]].Count);
+
+            Assert.IsTrue(dict[functions[0]].Contains(functions[1]));
+            Assert.IsTrue(dict[functions[1]].Contains(functions[1]));
+            Assert.IsTrue(dict[functions[2]].Contains(functions[1]));
+        }
+
+        /*
+         * fun a()
+         * {
+         *   b();
+         *   fun c()
+         *   {
+         *      c();
+         *   }
+         * }
+         * fun b()
+         * {
+         *   a();
+         * }
+         */
+        [TestMethod]
+        public void TestInnerFunction()
+        {
+            var functions = new List<FunctionDeclaration>();
+            var aCall = new FunctionCall("a", new List<Expression>());
+            var b = new FunctionDeclaration(
+                    "b",
+                    UnitType.Instance,
+                    new List<VariableDeclaration>(),
+                    new InstructionBlock(new List<Expression> { aCall }));
+            functions.Add(b);
+
+            var cCall = new FunctionCall("c", new List<Expression>());
+
+            var c = new FunctionDeclaration(
+                    "c",
+                    UnitType.Instance,
+                    new List<VariableDeclaration>(),
+                    new InstructionBlock(new List<Expression> { cCall }));
+
+            var bCall = new FunctionCall("b", new List<Expression>());
+
+            var a = new FunctionDeclaration(
+                    "a",
+                    UnitType.Instance,
+                    new List<VariableDeclaration>(),
+                    new InstructionBlock(new List<Expression> { bCall, c }));
+            functions.Add(a);
+
+            var root = new Program(new List<FunctionDeclaration> { a, b });
+
+            aCall.Declaration = a;
+            bCall.Declaration = b;
+            cCall.Declaration = c;
+
+            CallGraphGenerator gen = new CallGraphGenerator();
+            var dict = gen.BuildCallGraph(root);
+
+            Assert.AreEqual(1, dict[b].Count);
+            Assert.AreEqual(1, dict[a].Count);
+            Assert.AreEqual(1, dict[c].Count);
+
+            Assert.IsTrue(dict[b].Contains(a));
+            Assert.IsTrue(dict[c].Contains(c));
+            Assert.IsTrue(dict[a].Contains(b));
+        }
+    }
+}
