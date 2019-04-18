8711e8538e94dbe3db75edc029931129bc439ab6
Vladyslav Hlembotskyi <nobikik9@gmail.com>
2019-04-18 18:24:21 +0200

    RegisterAllocator API fixes

--- src/KJU.Core/CodeGeneration/RegisterAllocation/IRegisterAllocator.cs
+++ src/KJU.Core/CodeGeneration/RegisterAllocation/IRegisterAllocator.cs
@@ -1,9 +1,13 @@
 namespace KJU.Core.CodeGeneration.RegisterAllocation
 {
+    using System.Collections.Generic;
+    using Intermediate;
     using LivenessAnalysis;
 
     public interface IRegisterAllocator
     {
-        RegisterAllocationResult Allocate(InterferenceCopyGraphPair query);
+        RegisterAllocationResult Allocate(
+            InterferenceCopyGraphPair query,
+            IReadOnlyList<HardwareRegister> allowedHardwareRegisters);
     }
 }
--- src/KJU.Core/CodeGeneration/RegisterAllocation/RegisterAllocator.cs
+++ src/KJU.Core/CodeGeneration/RegisterAllocation/RegisterAllocator.cs
@@ -1,10 +1,14 @@
 namespace KJU.Core.CodeGeneration.RegisterAllocation
 {
+    using System.Collections.Generic;
+    using Intermediate;
     using LivenessAnalysis;
 
     public class RegisterAllocator : IRegisterAllocator
     {
-        public RegisterAllocationResult Allocate(InterferenceCopyGraphPair query)
+        public RegisterAllocationResult Allocate(
+            InterferenceCopyGraphPair query,
+            IReadOnlyList<HardwareRegister> allowedHardwareRegisters)
         {
             throw new System.NotImplementedException();
         }
