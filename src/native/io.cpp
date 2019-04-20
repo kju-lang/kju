c539bbba4c7ed63791880cf73382c444585df355
Michał Zieliński <michal@zielinscy.org.pl>
2019-04-20 23:14:19 +0200

    Kju IO

---
+++ src/native/io.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace KJU {
+__attribute__((sysv_abi))
+long long read() {
+    long long result;
+    #ifdef _WIN32
+    int r = scanf("%I64d", &result);
+    #else
+    int r = scanf("%lld", &result);
+    #endif
+    if (r != 1) {
+        fprintf(stderr, "unexpected end of input\n");
+        abort();
+    }
+    return result;
+}
+
+__attribute__((sysv_abi))
+void write(long long val) {
+    #ifdef _WIN32
+    printf("%I64d\n", val);
+    #else
+    printf("%lld\n", val);
+    #endif
+}
+}
+
